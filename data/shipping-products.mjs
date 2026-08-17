fdef90c0d2d3c83871104844c132f2dc546d195f
xpdistribuidoraoficial-jpg <xpdistribuidora.oficial@gmail.com>
2026-08-17 18:04:15 -0300

    feat: add shipping data for pilot products

---
+++ data/shipping-products.mjs
@@ -0,0 +1,24 @@
+export const shippingProducts = Object.freeze({
+  "casa-balanca-digital-cozinha-10kg": Object.freeze({
+    sku: "CASA-BALANCA-10KG",
+    category: "Cozinha",
+    price: 29.90,
+    stock: 5,
+    weight: 0.36,
+    length: 24.5,
+    width: 19,
+    height: 4.4,
+    fragile: false
+  }),
+  "casa-bomba-eletrica-garrafa-agua": Object.freeze({
+    sku: "CASA-BOMBA-AGUA-USB",
+    category: "Utilidades Domésticas",
+    price: 32.90,
+    stock: 5,
+    weight: 0.40,
+    length: 14,
+    width: 8,
+    height: 9,
+    fragile: false
+  })
+});
